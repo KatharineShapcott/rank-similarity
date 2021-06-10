2fc17cc164ae37cd0873c931ebb32759e986266f
Katharine Shapcott <katharine.shapcott@esi-frankfurt.de>
2021-06-10 15:40:23 +0200

    Add files for pip

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # rank-similarity
+
+Rank Similarity is a set of non-linear classification and transform tools for large datasets with many features that use the scikit-learn API. 
+
+# Installation
+### Dependencies
+rank-similarity requires:
+
+- Scikit-learn (>= 0.23)
+- Python (>= 3.7)
+- NumPy (>= 1.14.6)
+- SciPy (>= 1.1.0)
+
+Optionally for plotting examples:
+- matplotlib (>= 2.2.2)
+
+### Install via pip
+
+```
+pip install rank-similarity
+```
+
+### Development version
+
+To get the latest development version, clone the GitHub repository:
+
+```
+git clone https://github.com/KatharineShapcott/rank-similarity
+```
+
+# Usage
+
+``` python
+from ranksim import RankSimilarityClassifier
+X = [[0, 1], [1, 0]]
+y = [0, 1]
+clf = RankSimilarityClassifier()
+clf.fit(X, y)
+pred = clf.predict(X)
+```
+
+# Documentation
+
+More details and background information is available in the
+[online documentation]().
+
+# Citation
+
+Please cite the following publication (in preparation) 
---
+++ setup.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import codecs
+import os
+
+from setuptools import find_packages, setup
+
+# get __version__ from _version.py
+ver_file = os.path.join('ranksim', '_version.py')
+with open(ver_file) as f:
+    exec(f.read())
+
+DISTNAME = 'rank-similarity'
+DESCRIPTION = 'Rank Similarity is a set of non-linear classification and transform tools for large datasets. '
+with codecs.open('README.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+MAINTAINER = 'Katharine Shapcott'
+MAINTAINER_EMAIL = 'katharine.shapcott@esi-frankfurt.de'
+URL = 'https://github.com/KatharineShapcott/rank-similarity'
+LICENSE = 'new BSD'
+DOWNLOAD_URL = 'https://github.com/KatharineShapcott/rank-similarity'
+VERSION = __version__
+INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn']
+CLASSIFIERS = ['Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9']
+EXTRAS_REQUIRE = {
+    'tests': [
+        'pytest'],
+    'docs': [
+        'sphinx',
+        'sphinx-gallery',
+        'sphinx_rtd_theme',
+        'numpydoc',
+        'matplotlib'
+    ]
+}
+
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type="text/markdown",
+      zip_safe=True,  # the package can run out of an .egg file
+      classifiers=CLASSIFIERS,
+      packages=find_packages(),
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE)
